c00232348f34a72b655f8711d4bae4b616f22ca4
Jeffrey Arcand <jeffrey.arcand@ieee.org>
2012-10-11 05:44:40 -0400

    Changes to display of logged-in player info.

--- l/view.inc.php
+++ l/view.inc.php
@@ -13,10 +13,19 @@ function mp($main_body) {
 	$contents .= file_get_contents(dirname(__FILE__) . '/../style/bottom.inc');
 	
 	if ($_p) {
-		$user_html = sPrintF('<li>#</li><li><a href="${ROOT}/profile">%s</a></li><li><a href="${ROOT}/logout">Logout</a></li>',
-		  $_p['dname']);
+		if ($_p['credits'] == 200) {
+			$limit = 'Unlimited';
+		} else if ($_p['credits'] == 1) {
+			$limit = '1 Major';
+		} else {
+			$limit = $_p['credits'] . ' Majors';
+		}
+		$user_html = sPrintF('<li><a href="${ROOT}/profile">%1$s</a></li>
+<li><a href="${ROOT}/logout">Logout</a></li>',
+		  $_p['dname'], $limit);
 	} else {
-		$user_html = '<li>#</li><li><a href="${ROOT}/login">Login</a></li>';
+		$user_html = '<li>Welcome Guest!</li>
+<li><a href="${ROOT}/login">Login</a></li>';
 	}
 	$contents = str_replace('${CURR_USER}', $user_html, $contents);
 	$contents = str_replace('${ROOT}', $config['ROOT'], $contents);
