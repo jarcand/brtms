cb789e424f090b125565ae1b0d60eabfb593a067
Jeffrey Arcand <jeffrey.arcand@ieee.org>
2012-10-13 02:21:22 -0400

    Added missing config file, trying again.

---
+++ l/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$config = array();
+
+$config['instance'] = 'instance1';
+$config['SALT'] = '3eb9441e25';
+$config['ROOT'] = '';
+
+$config['DBHOST'] = '';
+$config['DBUSER'] = '';
+$config['DBPASS'] = '';
+$config['DBNAME'] = '';
+
+require_once dirname(__FILE__) . '/../config.inc.php';
+
