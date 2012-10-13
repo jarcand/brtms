c4ba719661517aacde1f37104a74d7f7924ed4cb
Jeffrey Arcand <jeffrey.arcand@ieee.org>
2012-10-13 02:02:58 -0400

    Added support for emailing the invitations.

--- t/sendinvite.php
+++ t/sendinvite.php
@@ -16,12 +16,17 @@ if (!$res) {
 $p = $res->fetch_assoc();
 
 // -----------------------------------------------------------------------------
-$headers = array(
+$to = sPrintF('%1$s %2$s &lt;%3$s&gt;', $p['fname'], $p['lname'], $p['email']);
+$to = sPrintF('%1$s %2$s &lt;%3$s&gt;', $p['fname'], $p['lname'], 'jeffrey@battleroyale.ca');
+$subject = 'Your Invitation to the Battle Royale Players Portal';
+// -----------------------------------------------------------------------------
+$headers = implode("\r\n", array(
 	'From: invite-bot@battleroyale.ca',
 	'Reply-To: accounts@battleroyale.ca',
-	'Bcc: accounts@battleroyale.ca',
+	'CC: accounts@battleroyale.ca',
+	'Content-Type: text/plain; charset=ISO-8859-1',
 	'X-Mailer: PHP/' . phpversion(),
-);
+));
 // -----------------------------------------------------------------------------
 
 
@@ -30,7 +35,7 @@ $remaining = $date - time();
 $remaining_days = floor($remaining / 3600 / 24);
 
 // -----------------------------------------------------------------------------
-$msg = sPrintF('Hello %1$s %2$s!
+$message = sPrintF('Hello %1$s %2$s!
 
 Thank you for purchasing a ticket for Battle Royale VI, happening
 November 3-4, 2012.  That is in just %4$s days!
@@ -55,9 +60,25 @@ Battle Royale Organizing Committee
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	
+	$result = @mail($to, $subject, str_replace('<br />', "\n", $message), $headers);
 	
-	
-	
+	if ($result) {
+
+		header('Content-Type: text/plain');
+		echo 'Email invitation successfully sent!';
+		
+		$res = $db->query($sql = sPrintF('UPDATE `players`
+		  SET `invitedts`=NOW()
+		  WHERE `pid`=' . s($pid)));
+		if (!$res) {
+			error($sql);
+		}
+		echo '  Database succesSfully updated!';
+		
+	} else {
+		header('Content-Type: text/plain');
+		echo 'Oh noes!  There were errors!';
+	}
 	
 } else {
 
@@ -66,15 +87,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <h1>Send Invitation Email</h1>
 <form action="#" method="post">
 <table>
-<tr><td>To:</td><td><input type="text" name="to" readonly="readonly" size="60" value="%1$s %2$s &lt;%3$s&gt;" /></td></tr>
-<tr><td>Subject:</td><td><input type="text" name="subject" readonly="readonly" size="60" value="Your Invitation to the Battle Royale Players Portal" /></td></tr>
-<tr><td>Headers:</td><td><textarea cols="40" readonly="readonly" rows="3" style="font-family:sans-serif;font-size:9pt;">%4$s</textarea></td></tr>
-<tr><td>Message:</td><td><textarea cols="60" readonly="readonly" rows="20" style="font-family:sans-serif;font-size:9pt;">%5$s</textarea></td></tr>
-<tr><td></td><td><input type="submit" value="Send Email Invitation" /> %6$s</td></tr>
+<tr><td>To:</td><td><input type="text" name="to" readonly="readonly" size="60" value="%1$s" /></td></tr>
+<tr><td>Subject:</td><td><input type="text" name="subject" readonly="readonly" size="60" value="%2$s" /></td></tr>
+<tr><td>Headers:</td><td><textarea cols="40" readonly="readonly" rows="4" style="font-family:sans-serif;font-size:9pt;">%3$s</textarea></td></tr>
+<tr><td>Message:</td><td><textarea cols="60" readonly="readonly" rows="20" style="font-family:sans-serif;font-size:9pt;">%4$s</textarea></td></tr>
+<tr><td></td><td><input type="submit" value="Send Email Invitation" /> %5$s</td></tr>
 </table>
 </form>
 </div>
-', $p['fname'], $p['lname'], $p['email'], implode("\r\n", $headers), $msg, $p['invitedts']);
+', $to, $subject, $headers, $message, $p['invitedts']);
 	
 	mp($src);
 }
