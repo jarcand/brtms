0ce607bad9249ab762bf16497275251f96e55943
Jeffrey Arcand <jeffrey.arcand@ieee.org>
2012-10-19 00:37:54 -0400

    Added link for send invite again.

--- t/players_list.php
+++ t/players_list.php
@@ -101,8 +101,8 @@ while ($p = $res->fetch_assoc()) {
 		$inv_src = sPrintF('<a href="sendinvite?pid=%1$s">Send Invite</a>', $p['pid']);
 	}
 	$inv_src2 = fd($p['firstlogints']);
-	if (!$inv_src) {
-		$inv_src = sPrintF('<a href="sendinvite?pid=%1$s">Send Invite Again</a>', $p['pid']);
+	if (!$inv_src2) {
+		$inv_src2 = sPrintF('<a href="sendinvite?pid=%1$s">Send Invite Again</a>', $p['pid']);
 	}
 	$src .= sPrintF('<tr><td>%s</td><td>%s %s</td><td>%s/%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>
 ', $p['pid'], $p['fname'], $p['lname'], $p['tours_major'], $p['credits'], $p['tours_crowd'], $p['teams'], $p['email'], fd($p['registeredts']), $inv_src, $inv_src2);
