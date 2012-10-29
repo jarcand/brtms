80ad81fd659a785ba47a213a25c15237820b567a
Jeffrey Arcand <jeffrey.arcand@ieee.org>
2012-10-29 05:00:59 -0400

    Added prizes page.

---
+++ prizes.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once dirname(__FILE__) . '/l/view.inc.php';
+
+mp('
+<div class="center">
+<h1>Tournament and Raffle Prizes</h1>
+<p>Many of you have be asking about prizes, and we are now happy to announce them!<br />
+ This year, we\'ve got over $3,000 of prizes and counting!</p>
+
+<h2>Hardware Upgrades</h2>
+<ul class="spaced">
+<li>CSX Cooler Master Spartan Limited Edition Computer Case</li>
+<li>Razer BalckWidow Ultimate USB Mechanical Gaming Keyboard</li>
+<li>Logitech G9X Laser Mouse 5700 DPI Precision Grips USB<br />
+ <small>(Call of Duty Modern Warfare 3 Edition)</small></li>
+<li>Sennheiser PC320 Gaming Stereo Headset w/ Noise Canceling Micropone</li>
+<li>Corsair Vengeance 8GB 2X4GB DDR3-1600 Dual Channel Memory Kits</li>
+<li>Kingston DataTraveler Ultimate G2 16GB USB3.0 Flash Drives</li>
+<li>Kingston DataTraveler 111 8GB USB 3.0 Flash Drives</li>
+</ul>
+
+<h2>Event Tickets</h2>
+<ul class="spaced">
+<li>SkyVenture Montreal, Vertical Wind Tunnel Simulated Sky Diving Ticket</li>
+<li>IEEE SPAC 2013 Tickets (March 2013)</li>
+<li>uOttawa Engineering Charity Ball 2013 Tickets<br />
+ <small>(including tickets to Gotham City pre-event, November 16 at Liquor Store)</small></li>
+<li>uOttawa Engineering DUSTED Tickets (November 10)</li>
+</ul>
+
+<h2>Gift Cards</h2>
+<ul class="spaced">
+<li>FutureShop $40 and $50 Gift Cards</li>
+</ul>
+
+<h2>Battle Royale Novelty Items</h2>
+<ul class="spaced">
+<li>Battle Royale VI Dog Tags</li>
+<li>Homemade Gaming-Themed Cakes</li>
+</ul>
+
+<h2>Cash Pots</h2>
+<ul class="spaced">
+<li>Up to $700 in cash prizes!</li>
+</ul>
+
+</div>
+', 'Tournament and Raffle Prizes');
+
