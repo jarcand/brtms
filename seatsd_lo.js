4ea0e005f0415827ce88ca4993929b6f18e24b21
Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
2013-01-17 02:24:44 -0500

    Continuing implementation of seat loader.

--- seatsd_lo.js
+++ seatsd_lo.js
@@ -11,7 +11,7 @@ function getSeats() {
 function loadSeats(data) {
 	var i = 0;
 	for (key in data) {
-		setTimeout(function() {loadSeat(key, data[key);}, i * 1000);
+		setTimeout(function() {loadSeat(key, data[key]);}, i * 1000);
 		i++;
 		if (i > 3)
 			break;
