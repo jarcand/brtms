4ac0ae720d1a2b462e5d2f8aff6b1aa1ace0bb49
Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
2013-01-17 02:17:37 -0500

    Added PHP pre-processing to JS.

---
+++ seatsd_l.js.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once dirname(__FILE__) . '/../l/config.inc.php';
+
+$contents = file_get_contents('seatsd_lo.js');
+$contents = str_replace('${ROOT}', $config['ROOT'], $contents);
+
+header('Content-Type: text/javascript');
+echo $contents;
+
