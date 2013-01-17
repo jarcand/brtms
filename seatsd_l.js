c3e35c3b52692355d4d64df4e04f7cac6bde35f2
Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
2013-01-17 02:15:14 -0500

    Started implementing loader.

--- seatsd_l.js
+++ seatsd_l.js
@@ -1,6 +1,20 @@
-function loadSeats() {
-	alert('Loaded');
+function getSeats() {
+	$.ajax({
+	  url: '${ROOT}/a/getseats',
+	  type: 'GET',
+	  dataType: 'json'
+	}).done(loadSeats).fail(function(jqSHR, textStatus) {
+		debug && alert(textStatus + ': ' + jqSHR.responseText);
+	});
 }
 
-loadSeats();
+function loadSeats(data) {
+	var src = '';
+	for (key in data) {
+		alert(key);
+		break;
+	}
+}
+
+getSeats();
 
